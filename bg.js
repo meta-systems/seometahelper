2119c3502d652517b1284ac3ecb66e315c3e4a2c
Nikolay Petrov <petrovnn@gmail.com>
2016-04-08 21:10:10 +0300

    initial

---
+++ bg.js
@@ -0,0 +1,5 @@
+﻿chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    chrome.pageAction.setIcon({tabId: tab.id,path:"icon16.png"});
+    chrome.pageAction.setTitle({tabId: tab.id,title:"SEO helper"});
+	chrome.pageAction.show(tab.id);
+});
