17d6297622a57f2c13dacc775d9a6ae0a13aed68
Nikolay Petrov <petrovnn@gmail.com>
2016-04-08 21:20:34 +0300

    some fixes

--- bg.js
+++ bg.js
@@ -1,5 +1,5 @@
 ﻿chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     chrome.pageAction.setIcon({tabId: tab.id,path:"icon16.png"});
-    chrome.pageAction.setTitle({tabId: tab.id,title:"SEO helper"});
+    chrome.pageAction.setTitle({tabId: tab.id,title:"SEO Meta Helper"});
 	chrome.pageAction.show(tab.id);
 });
